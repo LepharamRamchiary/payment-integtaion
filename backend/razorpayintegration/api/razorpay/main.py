11658171a616c5f28bc4b493f74619fc0060cb28
LepharamRamchiary <lepharamchiary@gmail.com>
2025-01-07 15:05:10 +0530

    add model tarnsction

--- backend/razorpayintegration/api/razorpay/main.py
+++ backend/razorpayintegration/api/razorpay/main.py
@@ -5,8 +5,8 @@ from rest_framework import status
 class RazorpayClient: 
     def create_order(self, amount, currency):
         data = {
-            "amount": 110,
-            "currency": "INR",
+            "amount": amount,
+            "currency": currency,
         }
         try:
             order_data = client.order.create(data=data)
@@ -16,5 +16,21 @@ class RazorpayClient:
                 {
                     "status_code": status.HTTP_400_BAD_REQUEST,
                     "message": e
+                }
+            )
+            
+
+    def verify_payment(self, razorpay_order_id, razorpay_payment_id, razorpay_signature):
+        try:
+            return client.utility.verify_payment_signature({
+                'razorpay_order_id': razorpay_order_id,
+                'razorpay_payment_id': razorpay_payment_id,
+                'razorpay_signature': razorpay_signature
+            })
+        except Exception as e:
+            raise ValidationError(
+                {
+                    'status_code': status.HTTP_400_BAD_REQUEST,
+                    'message': e
                 }
             )
--- backend/razorpayintegration/models.py
+++ backend/razorpayintegration/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Transaction(models.Model):
+    payment_id = models.CharField(max_length=255, verbose_name="Payment ID")
+    order_id = models.CharField(max_length=255, verbose_name="Order ID")
+    signature = models.CharField(max_length=255, verbose_name="Signature")
+    amount = models.IntegerField(verbose_name="Amount")
+    datetime = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return str(self.id)
